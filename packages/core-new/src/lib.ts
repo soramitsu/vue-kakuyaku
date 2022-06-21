30d71d240cb7603e6372c74918c0c6d234c347d7
0x009922 <a.marcius26@gmail.com>
2022-06-21 13:06:36 +0300

    refactor: improve design

--- packages/core-new/src/lib.ts
+++ packages/core-new/src/lib.ts
@@ -1,43 +1,86 @@
 import { Ref, watch, shallowRef, onScopeDispose, markRaw, shallowReactive, ref, unref } from 'vue'
 import { MaybeRef, useIntervalFn } from '@vueuse/core'
 
+/**
+ * Function that is wrapped into a {@link Task}.
+ *
+ * Accepts `onAbort
+ */
 export type TaskFn<T> = (onAbort: OnAbortFn) => Promise<T>
 
 export type OnAbortFn = (fn: () => void) => void
 
-export type FalsyScopeKey = false | null | undefined
+/**
+ * Atomic task state. Represented as an algebraic enumerated type to be more type-strong.
+ */
+export type TaskState<T> = TaskStateUninit | TaskStatePending | TaskResult<T> | TaskStateAborted
 
-export type ScopeKey = string | number | symbol
-
-export type TaskState<T> = TaskStateUninit | TaskStatePending | TaskStateOk<T> | TaskStateErr
-
+/**
+ * Used to build algebraic enumerated types
+ */
 export interface Tagged<T extends string> {
   readonly kind: T
 }
 
+/**
+ * Task is not yet initialized
+ */
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface TaskStateUninit extends Tagged<'uninit'> {}
 
+/**
+ * Task is pending
+ */
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface TaskStatePending extends Tagged<'pending'> {}
 
-export interface TaskStateOk<T> extends Tagged<'ok'> {
+/**
+ * Task is aborted
+ */
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface TaskStateAborted extends Tagged<'aborted'> {}
+
+/**
+ * Ok or Err
+ */
+export type TaskResult<T> = TaskResultOk<T> | TaskResultErr
+
+/**
+ * Task succeeded. Stores its execution result.
+ */
+export interface TaskResultOk<T> extends Tagged<'ok'> {
   readonly result: T
 }
-export interface TaskStateErr extends Tagged<'err'> {
+
+/**
+ * Task errored. Stores its error.
+ */
+export interface TaskResultErr extends Tagged<'err'> {
   readonly error: unknown
 }
-
-export const ANONYMOUS_SCOPE = Symbol('Anonymous')
 
 function stateRaw<T extends TaskState<any>>(state: T): T {
   return markRaw(state)
 }
 
-const TASK_RUN_ABORT = Symbol('Aborted')
-const STATE_UNINIT = Object.freeze(stateRaw({ kind: 'uninit' }))
-const STATE_PENDING = Object.freeze(stateRaw({ kind: 'pending' }))
+/**
+ * Freezed `uninit` state.
+ */
+export const STATE_UNINIT = Object.freeze(stateRaw({ kind: 'uninit' }))
 
+/**
+ * Freezed `pending` state.
+ */
+export const STATE_PENDING = Object.freeze(stateRaw({ kind: 'pending' }))
+
+/**
+ * Freezed `aborted` state.
+ */
+export const STATE_ABORTED = Object.freeze(stateRaw({ kind: 'aborted' }))
+
+/**
+ * Simplified version of [`AbortController`](https://developer.mozilla.org/en-US/docs/Web/API/AbortController)
+ */
 class AbortHandle {
   public readonly onAbort: OnAbortFn
   private hooks: (() => void)[] = []
@@ -53,26 +96,172 @@ class AbortHandle {
       try {
         fn()
       } catch (err) {
-        console.error('Async action abort hook errored:', err)
+        console.error('Abortation hook errored:', err)
       }
     }
   }
 }
 
 /**
- * Basic abstraction around **redoable headless async operation**.
+ * Stateless abstraction around
+ */
+export interface BareTask<T> {
+  run: () => Promise<BareTaskRunResult<T>>
+  abort: () => void
+}
+
+export type BareTaskRunResult<T> = TaskStateAborted | TaskResult<T>
+
+/**
+ * Create a stateless (thus reactivity-free) task. Useful even out of scope of Vue Reactivity.
+ * @param fn
+ * @returns
+ */
+export function createBareTask<T>(fn: TaskFn<T>): BareTask<T> {
+  let active: null | { abortHandle: AbortHandle; promise: Promise<unknown> } = null
+
+  function run(): Promise<BareTaskRunResult<T>> {
+    abort()
+
+    const abortHandle = new AbortHandle()
+
+    const promise = new Promise<BareTaskRunResult<T>>((resolve) => {
+      abortHandle.onAbort(() => resolve(STATE_ABORTED))
+      fn(abortHandle.onAbort)
+        .then((result) => resolve({ kind: 'ok', result }))
+        .catch((error) => resolve({ kind: 'err', error }))
+    })
+
+    active = { promise, abortHandle }
+
+    return promise
+  }
+
+  function abort() {
+    if (active) {
+      active.abortHandle.abort()
+      active = null
+    }
+  }
+
+  return { run, abort }
+}
+
+/**
+ * Statefull abstraction around **redoable headless async operation**.
  *
- * **Async operation** - async function or function that returns a Promise.
+ * What does it mean:
  *
- * **Headless** means that this operation doesn't require any arguments.
+ * - **async operation** - a function that returns a Promise (same as `async` function)
+ * - **headless** - operation doesn't require any parameters
+ * - **redoable** - operation could be re-run, which allows to build useful utilities around it
  *
- * **Redoability** allows to build usefull utilities around a task, like error retry.
+ * `Task` stores its current, *atomic* state only. It doesn't store it's last result or error even if
+ * it is pending at the moment. You can achieve it with utilities like {@link usePassiveState} or {@link useLastTaskResult}.
  */
 export interface Task<T> {
+  /**
+   * Atomic task state
+   */
   state: TaskState<T>
   run: () => void
   abort: () => void
 }
+
+/**
+ * Setup a task with reactive state and disposition on scope dispose.
+ *
+ * Receives the only one argument - {@link TaskFn `TaskFn<T>`}.
+ *
+ * Task is aborted when:
+ *
+ * - {@link Task.run `.run()`} is called while task is pending
+ * - {@link Task.abort `.abort()`} is called
+ * - task's scope is disposed
+ *
+ * ## Examples
+ *
+ * ### Making a side-effect
+ *
+ * ```ts
+ * const task = useTask(async () => {
+ *   await delay(200)
+ *   if (Math.random() > 0.42) throw new Error('oops')
+ * })
+ *
+ * function doSomething() {
+ *   task.run()
+ * }
+ *
+ * const error = computed(() =>
+ *   task.state.kind === 'err'
+ *     ? task.state.error
+ *     : null
+ * )
+ * ```
+ *
+ * ### Loading something - handling abortation
+ *
+ * ```ts
+ * interface User {
+ *   name: string
+ * }
+ *
+ * const task = useTask<User[]>(async (onAbort) => {
+ *   const controller = new AbortController();
+ *   onAbort(() => controller.abort())
+ *   return fetch('/users', { signal: controller.signal })
+ * })
+ *
+ * task.run()
+ *
+ * const isPending = computed(() => task.state.kind === 'pending')
+ * const users = computed<null | User[]>(() =>
+ *   task.state.kind === 'ok'
+ *     ? task.state.result
+ *     : null
+ * )
+ * ```
+ *
+ * ## Useful utilities
+ *
+ * - {@link useLastTaskResult}
+ * - {@link usePassiveState}
+ * - {@link useErrorRetry}
+ * - {@link useScope} - when you need to conditionaly **setup** a task
+ * - {@link useDanglingScope} - when you need to setup a task, but later, e.g. on some event
+ * - {@link createBareTask} - when you don't need task reactive state, but want to have redoability, abortation
+ *   and {@link TaskResult<T>}
+ */
+export function useTask<T>(fn: TaskFn<T>): Task<T> {
+  const bare = createBareTask(fn)
+  const { abort } = bare
+
+  const task: Task<T> = shallowReactive({
+    state: STATE_UNINIT,
+    run,
+    abort: abort,
+  })
+
+  function run() {
+    abort()
+    task.state = STATE_PENDING
+    bare.run().then((result) => {
+      task.state = result
+    })
+  }
+
+  onScopeDispose(abort)
+
+  return task
+}
+
+/**
+ * Used for scopes utilities. Primitive type (same as Vue's `:key`) to distinguish scopes from each other.
+ */
+export type ScopeKey = string | number | symbol
+
+export type FalsyScopeKey = false | null | undefined
 
 export interface ScopeSetup<T> {
   setup: T
@@ -82,6 +271,74 @@ export interface KeyedScopeSetup<T, K extends ScopeKey> extends ScopeSetup<T> {
   key: K
 }
 
+/**
+ * Uses [`EffectScope` API](https://vuejs.org/api/reactivity-advanced.html#effectscope)
+ * to setup a scope conditionally or binded to a key.
+ *
+ * Useful when:
+ *
+ * - you need to setup a task conditionally
+ * - you need to setup a task according to some key
+ *
+ * note: actually it works not only with tasks.
+ *
+ * ## Examples
+ *
+ * ### Fetch data on some condition
+ *
+ * ```ts
+ * const shouldFetch = ref(false)
+ *
+ * const scope = useScope(shouldFetch, () => {
+ *   const task = useTask(async () => fetch('/users'))
+ *
+ *   // do any convenient stuff around the task
+ *   useErrorRetry(task)
+ *   const state = usePassiveState(task)
+ *   const users = computed(() => state.result?.some)
+ *   const pending = computed(() => state.pending)
+ *   task.run()
+ *
+ *   // return anything you are comfort with
+ *   return { users, pending }
+ * })
+ *
+ * // no scope until `shouldFetch` is false
+ * scope.value === null
+ *
+ * // enabling scope
+ * shouldFetch.value = true
+ *
+ * // *reactivity magic happens...*
+ *
+ * // Voila! The scope with all internal stuff is set up
+ * console.log(scope.value.setup.pending.value)
+ * console.log(scope.value.setup.users.value)
+ * ```
+ *
+ * ### Fetch user by id
+ *
+ * ```ts
+ * const userId = ref(41)
+ *
+ * const scope = useScope(
+ *   userId,
+ *   // you can access the key that is associated with the scope that is being setup
+ *   (resolvedUserId) => {
+ *     const task = useTask(async () => fetch(`/users/${resolvedUserId}`));
+ *     return task
+ *   }
+ * )
+ *
+ * // when key always exists, `scope.value` is never a null
+ * console.log(scope.value.setup.state) // outputs task's current state
+ * console.log(scope.value.key) // outputs active scope's key
+ *
+ * ## See Also
+ *
+ * - {@link useDanglingScope} - if you need to setup a scope, binded to current one, **but later**.
+ * ```
+ */
 export function useScope<T>(cond: Ref<boolean>, setup: () => T): Ref<null | ScopeSetup<T>>
 export function useScope<T, K extends ScopeKey>(key: Ref<K>, setup: (key: K) => T): Ref<KeyedScopeSetup<T, K>>
 export function useScope<T, K extends ScopeKey>(
@@ -94,77 +351,53 @@ export function useScope<T>(
   setup: (key?: ScopeKey) => T,
 ): Ref<null | ScopeSetup<T> | KeyedScopeSetup<T, ScopeKey>> {}
 
-/**
- * Setup a task
- */
-export function useTask<T>(fn: TaskFn<T>): Task<T> {
-  interface Active {
-    promise: Promise<void>
-    abort: AbortHandle
-  }
-  let active: null | Active = null
-
-  const task: Task<T> = shallowReactive({
-    state: STATE_UNINIT,
-    run,
-    abort: doAbort,
-  })
-
-  function run(abort = true) {
-    abort && doAbort()
-
-    const abortHandle = new AbortHandle()
-    const thisPromise = new Promise<T | typeof TASK_RUN_ABORT>((resolve, reject) => {
-      task.state = STATE_PENDING
-      abortHandle.onAbort(() => resolve(TASK_RUN_ABORT))
-      fn(abortHandle.onAbort).then(resolve).catch(reject)
-    })
-      .then((result) => {
-        if (active?.promise === thisPromise) {
-          if (result === TASK_RUN_ABORT) {
-            task.state = STATE_UNINIT
-          } else {
-            task.state = stateRaw({ kind: 'ok', result })
-          }
-        }
-      })
-      .catch((error) => {
-        if (active?.promise === thisPromise) {
-          task.state = stateRaw({ kind: 'err', error })
-        }
-      })
-    active = { promise: thisPromise, abort: abortHandle }
-  }
-
-  function doAbort() {
-    if (active) {
-      active.abort.abort()
-      active = null
-    }
-  }
-
-  onScopeDispose(doAbort)
-
-  return task
-}
-
 export interface DanglingScope<T> {
-  scope: Ref<null | ScopeSetup<T>>
+  scope: Readonly<Ref<null | ScopeSetup<T>>>
   setup: (fn: () => T) => void
   dispose: () => void
 }
 
+/**
+ * Setup a scope, but later
+ *
+ * ## Examples
+ *
+ * ```ts
+ * const submitAction = useDanglingScope<TaskMultiState<void>>();
+ *
+ * function submit(age: number, name: string) {
+ *   submitAction.setup(() => {
+ *     const task = useTask(async () => doAsyncStuff({ age, name }))
+ *     const passive = usePassiveState(task)
+ *     return passive
+ *   })
+ * }
+ *
+ * function cleanAction() {
+ *   submitAction.dispose()
+ * }
+ *
+ * watchEffect(() => {
+ *   if (submitAction.scope) {
+ *     console.log('pending:', submitAction.scope.setup.pending)
+ *   }
+ * })
+ * ```
+ */
 export function useDanglingScope<T>(): DanglingScope<T> {}
 
 export type Maybe<T> = null | { some: T }
 
-export interface TaskInertState<T> {
+/**
+ * It is *super* because it stores task's result, error and pending states simultaneously.
+ */
+export interface TaskSuperState<T> {
   result: Maybe<T>
   error: Maybe<unknown>
   pending: boolean
 }
 
-function stateToInert<T>(state: TaskState<T>): TaskInertState<T> {
+export function stateToSuper<T>(state: TaskState<T>): TaskSuperState<T> {
   return {
     result: state.kind === 'ok' ? { some: state.result } : null,
     error: state.kind === 'err' ? { some: state.error } : null,
@@ -172,8 +405,12 @@ function stateToInert<T>(state: TaskState<T>): TaskInertState<T> {
   }
 }
 
-export function useInertState<T>(task: Task<T>): TaskInertState<T> {
-  const state: TaskInertState<T> = shallowReactive(stateToInert(task.state))
+/**
+ * Creates a reactive multi state that is *passively* syncronized with task's atomic state.
+ * *Passive state* means it stores last task result until it succeed again.
+ */
+export function usePassiveState<T>(task: Task<T>): TaskSuperState<T> {
+  const state: TaskSuperState<T> = shallowReactive(stateToSuper(task.state))
 
   watch(
     () => task.state,
@@ -197,8 +434,13 @@ export function useInertState<T>(task: Task<T>): TaskInertState<T> {
   return state
 }
 
-export function useLastTaskResult<T>(task: Task<T>): Ref<null | TaskStateOk<T> | TaskStateErr> {
-  const result = shallowRef<null | TaskStateOk<T> | TaskStateErr>(null)
+/**
+ *
+ * @param task
+ * @returns
+ */
+export function useLastTaskResult<T>(task: Task<T>): Ref<null | TaskResult<T>> {
+  const result = shallowRef<null | TaskResult<T>>(null)
 
   watch(
     () => task.state,
